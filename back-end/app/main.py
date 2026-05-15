baaa0f2c64afefc91d0e4cf89cfb227d7c9de254
Jonathan Araldi <jonathan.araldi@outlook.com>
2026-05-15 21:13:10 +0200

    fix: mise à jour des origines CORS pour la production

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -15,9 +15,14 @@ app = FastAPI(
 
 # CONFIGURATION DU MIDDLEWARE (CORS)
 # Autorise les requêtes provenant du Front-End (Vite/React)
+# Note : Obligation de lister les origins car allow_credentials=True
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=[
+        "https://m-motors-skies-land.netlify.app",
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
